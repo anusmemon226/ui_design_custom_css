b38b2bfc795f805b1f8f26794dac6e825c3cd2c5
anusmemon226 <anusm226@gmail.com>
2024-11-07 00:14:11 +0500

    Minor Bug Fixed

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -11,9 +11,9 @@ function AboutSection() {
         <p>
           There are many variations of passages of Lorem Ipsum available, but
           the majority have suffered alteration in some form, by injected
-          humour, or randomised words which don't look even slightly believable.
+          humour, or randomised words which dont look even slightly believable.
           If you are going to use a passage of Lorem Ipsum, you need to be sure
-          there isn't anything embarrassing hidden in the middle of text. All
+          there isnt anything embarrassing hidden in the middle of text. All
           the Lorem Ipsum generators on the Internet tend to repeat predefined
           chunks as necessary, making this the first true generator on the
           Internet.
--- src/components/Toggler.tsx
+++ src/components/Toggler.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBarToggler from "../css/NavbarStyling/Toggler.module.css"
-function Toggler({showToggler,setShowToggler}:{showToggler:boolean,setShowToggler:Function}) {
+function Toggler({showToggler,setShowToggler}:{showToggler:boolean,setShowToggler:(status:boolean)=>void}) {
   return (
     <div
       className={NavBarToggler.navbarToggler}
