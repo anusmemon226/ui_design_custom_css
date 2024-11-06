47a3916071b9b924b1a500a47e95a2d7a8134066
anusmemon226 <anusm226@gmail.com>
2024-11-07 00:29:11 +0500

    Minor Bug Fixed

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div style={{padding:"80px 20px",width:"85%",margin:"auto"}}>
+      <h1>404 Page Not Found</h1>
+    </div>
+  );
+}
+
+export default NotFound;
