1337ad3a461a11081694cb1a07241dfe5326cb16
anusmemon226 <anusm226@gmail.com>
2024-11-07 01:18:26 +0500

    Minor Bug Fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-// import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Navbar";
 // import Footer from "@/components/Footer";
 
 const geistSans = localFont({
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {/* <Navbar/> */}
+        <Navbar/>
         {children}
         {/* <Footer/> */}
       </body>
